abf14c1cb02cb787e6f907bebc04adf3fc10d638
Yaxyobek0877 <106481261+Yaxyobek0877@users.noreply.github.com>
2023-11-09 09:25:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes,filters,MessageHandler
-
-
-ls=[""]
-
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_html("Menga shu bot orqali o'z yechimlaringni yuboringlar agar sizlar buyogdan turib otikizsalaring IP da ushalb qolyapdi")
-    for i in ls:
-        await update.message.reply_html(f"<code>{i}</code>")
-
-    await update.message.reply_text(f"masalalar soni hozrida: {len(ls)}")
-
-    await update.message.reply_html("Nima yossalaringham menga keladi. Agar men yana masalarni yozsam uni bilish uchun /start ni bosilar shunda men qoshgan masalarni hammasi keladi.")
+from telegram.ext import ApplicationBuilder, ContextTypes,filters,MessageHandler
 
 async def text(update:Update, context:ContextTypes.DEFAULT_TYPE) -> None:
     text=update.message.text
@@ -22,7 +10,6 @@ async def text(update:Update, context:ContextTypes.DEFAULT_TYPE) -> None:
 
 app = ApplicationBuilder().token("6647247101:AAEEjUX3QFqmFBJU7okbqz49oXlUpe0M-Zk").build()
 
-app.add_handler(CommandHandler("start", hello))
-app.add_handler(MessageHandler(filters.TEXT,text))
+app.add_handler(MessageHandler(filters.ALL,text))
 
 app.run_polling()
